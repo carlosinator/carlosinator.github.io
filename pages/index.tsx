0e2dc2889daed2e86d793b03b417392c929c03df
Carlos Vonessen <carlos.w2704@gmail.com>
2024-10-29 14:46:21 +0100

    fix: fix eslint nagging

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import Image from 'next/image'
-import Link from 'next/link'
 import { useState, useEffect } from 'react'
 import { updates, categoryColors } from '../data/updates'
 import { ExternalLink } from 'lucide-react'
